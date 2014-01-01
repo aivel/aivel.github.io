3598b458fcefc0c91c0b74762c3e0feb4677152f
Semen Danilov <samvimes@yandex.ru>
2014-01-01 14:06:07 +0400

    Style changes + added sliding on hover of navbar

--- js/main.js
+++ js/main.js
@@ -7,5 +7,14 @@ $(document).ready(function(e) {
 	// Change hash for page-reload - НАФИГА ЭТО?:) Но на SO так...
 	$('.navigation-tabs a').on('shown.bs.tab', function (e) {
 		window.location.hash = e.target.hash;
-	})    
+	})   
+	
+	$('.navbar .dropdown').hover(
+		function() {
+			$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+		}, function() {
+		  $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+		}
+	);
+ 
 });
